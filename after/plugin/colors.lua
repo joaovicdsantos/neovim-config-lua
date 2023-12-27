c521e4061946868686d8a49198433c3f498a962e
João Victor <joaovicdsantos@gmail.com>
2023-12-27 19:35:58 -0300

    feat: new config for 2024 :tada:

---
+++ after/plugin/colors.lua
@@ -0,0 +1,12 @@
+function ColorMyPencils(color)
+	color = color or "kanagawa"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+
+  vim.cmd([[hi CursorLine guibg=#403658]])
+  vim.cmd([[hi ColorColumn guibg=#26203f]])
+end
+
+ColorMyPencils()
