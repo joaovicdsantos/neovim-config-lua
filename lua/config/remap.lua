82f63b5b5a26d525ae88135c8e4f35bed3e1c065
joaovicdsantos <joaovicdsantos@gmail.com>
2024-01-13 17:54:18 -0300

    feat: add new remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -11,3 +11,8 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
