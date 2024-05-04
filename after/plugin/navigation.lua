733f7afe22fe8773b2e608ab1ae16aac7bde6b9e
joaovicdsantos <joaovicdsantos@gmail.com>
2024-05-04 19:01:55 -0300

    feat: add navigation

---
+++ after/plugin/navigation.lua
@@ -0,0 +1,10 @@
+-- Barbar
+vim.keymap.set("n", "<A-h>", "<Cmd>BufferPrevious<CR>")
+vim.keymap.set("n", "<A-l>", "<Cmd>BufferNext<CR>")
+vim.keymap.set("n", "<A-p>", "<Cmd>BufferPin<CR>")
+vim.keymap.set("n", "<A-c>", "<Cmd>BufferClose<CR>")
+vim.keymap.set("n", "<C-p>", "<Cmd>BufferPick<CR>")
+
+-- Neotree
+vim.keymap.set("n", "<leader>t", "<Cmd>Neotree<CR>")
+vim.keymap.set("n", "<leader>tc", "<Cmd>Neotree close<CR>")
--- lua/config/init.lua
+++ lua/config/init.lua
@@ -5,3 +5,5 @@ require("config.remap")
 -- Plugins \o/
 require("config.lazy")
 require("luasnip.loaders.from_vscode").lazy_load()
+
+vim.cmd([[ set shell=/bin/zsh ]])
