3dd93e93934f53dd6e188f28cea463d1adc6307a
joaovicdsantos <joaovicdsantos@gmail.com>
2024-05-27 21:24:00 -0300

    soo many thing...i don't remember, sorry

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,7 +1,8 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  require("lsp-format").on_attach(client, bufnr)
+  local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -26,7 +27,7 @@ lsp_zero.set_sign_icons({
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer', 'gopls'},
+  ensure_installed = { 'tsserver', 'rust_analyzer', 'gopls' },
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
@@ -37,7 +38,7 @@ require('mason-lspconfig').setup({
 })
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 -- this is the function that loads the extra snippets to luasnip
 -- from rafamadriz/friendly-snippets
@@ -47,12 +48,12 @@ local lspkind = require("lspkind")
 
 cmp.setup({
   sources = {
-    {name = 'path'},
-    {name = 'luasnip', keyword_length = 2},
-    {name = 'codeium' },
-    {name = 'nvim_lsp'},
-    {name = 'nvim_lua'},
-    {name = 'buffer', keyword_length = 3},
+    { name = 'path' },
+    { name = 'luasnip', keyword_length = 2 },
+    { name = 'codeium' },
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lua' },
+    { name = 'buffer',  keyword_length = 3 },
   },
   formatting = {
     format = lspkind.cmp_format({
